0c789f1e9570bbafc7447ed99dad8fc498aa244b
anakin-dabir <anakindabir@gmail.com>
2023-10-09 18:41:19 +0500

    srvice worket

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration.jsx';
+import * as serviceWorkerRegistration from '../serviceWorkerRegistration.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
